8a9fda613c1cd5ce5b19d0e496b5f88cff3fb89e
Anthony Salemo <ajssalemo@gmail.com>
2022-11-06 22:40:53 -0500

    adding nextjs

---
+++ nextjs/next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+    }
+  }
+}
+
+module.exports = nextConfig
